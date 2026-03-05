1b30d1fbe5d8310bdb0d563d17605be67cb18968
lagopede <lagopede334@hotmail.fr>
2026-03-05 12:27:52 -0500

    WIP first prototype, on good track

---
+++ scripts/calculator.js
@@ -0,0 +1,68 @@
+
+/*
+    Calculator data
+*/
+let calculatorData = {
+    firstOperand: undefined,
+    secondOperand: undefined,
+    operation: undefined,
+}
+
+
+
+
+/**
+    Calculator methods
+ */
+function operate(calculatorData) {
+
+    if (isNotOperatable(calculatorData)) {
+        return null; //TODO check it out later
+    }
+
+    switch (calculatorData.operation) {
+        case "+":
+            return calculatorData.firstOperand + calculatorData.secondOperand;
+
+        case "-":
+            return calculatorData.firstOperand - calculatorData.secondOperand;
+
+        case "*":
+            return calculatorData.firstOperand * calculatorData.secondOperand;
+
+        case "/":
+            return calculatorData.firstOperand / calculatorData.secondOperand;
+    }
+}
+
+function isNotOperatable(calculatorData) {
+    return isNaN(calculatorData.firstOperand) || isNaN(calculatorData.secondOperand) || calculatorData.operation == undefined
+}
+
+
+/*
+    Buttons methods
+*/
+function numberButtonClick(number) {
+    if(isNaN(number)){
+        return //TODO if someone modify the button to put something other than button
+    }
+
+    if(calculatorData.operation == undefined){ //still on first number
+        calculatorData.firstOperand = calculatorData.firstOperand ?  parseInt("" + calculatorData.firstOperand + number) : number;
+        console.log(calculatorData)
+    }
+    else {
+        calculatorData.secondOperand = calculatorData.secondOperand ?  parseInt("" + calculatorData.secondOperand + number) : number;
+        console.log(calculatorData)
+    }
+}
+
+
+function operationButtonClick(operation){
+    const acceptedOperations = "+-/*";
+
+    if(calculatorData.operation == undefined && acceptedOperations.includes(operation)) {
+        calculatorData.operation = operation;
+    }
+}
